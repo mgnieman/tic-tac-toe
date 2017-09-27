594cdcf7872ab706d9b27e73cd61e9c736e1fb59
mgnieman <michael.nieman@libertymutual.com>
2017-09-27 10:41:11 -0400

    Add .click functionality to grid
    Created a function, for each button, that will log "you clicked
    box[whatever]" to the console.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Here's where I'll document my (mis)adventures
+First thing I did was write the game logic in logic.js.  I have to manually enter an array, i.e. 'const array = [x, o, o, o, x, o, o, o, o]', and my code will log "true" or "false" for each of the 8 possible winning combinations (3 rows, 3 columns & 2 diagonals.)
+
+Next I used index.html & index.scss to build the 3x3 grid, viewable in a browser.  I used some bootstrap code.
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,7 +6,33 @@ const config = require('./config')
 $(() => {
   setAPIOrigin(location, config)
 })
-
+$('#box0').click(function () {
+  console.log('you clicked box0')
+})
+$('#box1').click(function () {
+  console.log('you clicked box1')
+})
+$('#box2').click(function () {
+  console.log('you clicked box2')
+})
+$('#box3').click(function () {
+  console.log('you clicked box3')
+})
+$('#box4').click(function () {
+  console.log('you clicked box4')
+})
+$('#box5').click(function () {
+  console.log('you clicked box5')
+})
+$('#box6').click(function () {
+  console.log('you clicked box6')
+})
+$('#box7').click(function () {
+  console.log('you clicked box7')
+})
+$('#box8').click(function () {
+  console.log('you clicked box8')
+})
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
